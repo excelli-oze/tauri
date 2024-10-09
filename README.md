9e8e2b4e57101ce5b2e4c7e6b1e703be080a9a3f
umeshchatla <umesh.chatla@excelli.com>
2024-10-09 13:24:18 +0530

    add barcode scanner plugin

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Development environment doesn't work in our selected browser, but the browser popped up when run `npm run tauri dev`
 - To open `inspector` in android and ios, https://v2.tauri.app/develop/#opening-the-web-inspector-1 
+- before running `npm run tauri android dev` start the android emulator in android studio, so that the tauri recognizes and auto isntalls app on it
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,14 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+    
+    #[cfg(any(target_os = "android", target_os = "ios"))]
+    {
+        builder = builder.plugin(tauri_plugin_barcode_scanner::init());
+    }
+    
+    builder
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
